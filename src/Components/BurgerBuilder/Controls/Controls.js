24565aa651a871e22e06e4fb71ba3d89b48c76b8
Toufik khan <toufik9909@gmail.com>
2023-05-30 21:49:11 +0600

    Add and Remove ingredients

--- src/Components/BurgerBuilder/Controls/Controls.js
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -11,14 +11,15 @@ const controls = [
 const BuildControl = props => {
     return (
         <div className="d-flex">
-            <div className="m-auto ml-5" style={{fontSize: "1.2rem", fontWeight: "bold"}}>{props.label}</div>
-            <button className="btn btn-danger btn-sm m-1" >less</button>
-            <button className="btn btn-success btn-sm m-1">more</button>
+            <div className="m-auto ml-5" style={{ fontSize: "1.2rem", fontWeight: "bold" }}>{props.label}</div>
+            <button className="btn btn-danger btn-sm m-1" onClick={props.removed} >less</button>
+            <button className="btn btn-success btn-sm m-1" onClick={props.added}>more</button>
         </div>
     )
 }
 
-const Controls = (props) => {
+
+const Controls = props => {
     return (
         <div className="container text-center mr-md-5">
             <Card style={{ margin: "30px 0", textAlign: "center", width: "400px" }}>
@@ -29,6 +30,9 @@ const Controls = (props) => {
                             label={item.label}
                             type={item.type}
                             key={Math.random}
+                            added={() => props.ingredientAdded(item.type)}
+                            removed={() => props.ingredientRemoved(item.type)}
+
                         />
                     })}
                 </CardBody>
