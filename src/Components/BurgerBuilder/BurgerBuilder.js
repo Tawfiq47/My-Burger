09bde0c2efa4eb093aaf971e37cf2c4b51b8d4fd
Toufik khan <toufik9909@gmail.com>
2023-05-30 13:08:37 +0600

    show ingredient according to state

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import Burger from "./Burger/Burger";
 
-export default class BurgerBuilder extends Component{
-    render(){
-        return(
+export default class BurgerBuilder extends Component {
+    state = {
+        ingredients: [
+            { type: 'cheese', amount: 0 },
+            { type: 'salad', amount: 0 },
+            { type: 'meat', amount: 0 },
+        ]
+    }
+    render() {
+        return (
             <div>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients} />
             </div>
         )
     }
-} 
+}
+
