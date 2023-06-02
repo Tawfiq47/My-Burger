c049bc43fcd8440cc51f59ae28fc83778ffdd016
Toufik khan <toufik9909@gmail.com>
2023-06-02 11:30:05 +0600

    adding redux problem

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -2,58 +2,42 @@ import React, { Component } from "react";
 import Burger from "./Burger/Burger";
 import Controls from "./Controls/Controls";
 import Summary from "./Summary/Summary";
-import { Modal, ModalBody, ModalHeader, ModalFooter, Button} from "reactstrap"; 
+import { Modal, ModalBody, ModalHeader, ModalFooter, Button } from "reactstrap";
 
+import { connect } from "react-redux";
+import { addIngredient, removeIngredient, updatePurchasable } from "../../redux/actionCreators";
 
-const INGREDIENT_PRICE = {
-    cheese: 20,
-    salad: 40,
-    meat: 90
+const mapStateToProps = (state) => {
+      
+     return {
+        ingredients: state.ingredients,
+        totalPrice: state.totalPrice,
+        purchasable: state.purchasable,
+    }
 }
 
+const mapDispatchToProps = (dispatch) => {
+     
+    return {
+        addIngredient: (igtype) => dispatch(addIngredient(igtype)),
+        removeIngredient: (igtype) => dispatch(removeIngredient(igtype)),
+        updatePurchasable: () => dispatch(updatePurchasable()),
+    }
+}
 
-export default class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     state = {
-        ingredients: [
-            { type: 'cheese', amount: 0 },
-            { type: 'salad', amount: 0 },
-            { type: 'meat', amount: 0 },
-        ],
-        totalPrice: 80,
         modalOpen: false,
-        purchasable: false,
     }
 
-    updatePurchasable = ingredients => {
-        const sum = ingredients.reduce((sum, element) => {
-            return sum + element.amount;
-        }, 0);
-        this.setState({ purchasable: sum > 0 })
-    }
 
     addIngredientHandle = type => {
-        const ingredients = [...this.state.ingredients];
-        const newPrice = this.state.totalPrice + INGREDIENT_PRICE[type];
-        for (let item of ingredients) {
-            if (item.type === type) item.amount++;
-        }
-        this.setState({ ingredients: ingredients, totalPrice: newPrice });
-        this.updatePurchasable(ingredients)
-
+        this.props.addIngredient(type);
+        this.props.updatePurchasable();
     }
     removeIngredientHandle = type => {
-        const ingredients = [...this.state.ingredients];
-        const newPrice = this.state.totalPrice - INGREDIENT_PRICE[type];
-
-        for (let item of ingredients) {
-            if (item.type === type) {
-                if (item.amount <= 0) return;
-                item.amount--;
-            }
-
-        }
-        this.setState({ ingredients: ingredients, totalPrice: newPrice });
-        this.updatePurchasable(ingredients)
+        this.props.removeIngredient(type);
+        this.props.updatePurchasable();
 
     }
 
@@ -64,33 +48,30 @@ export default class BurgerBuilder extends Component {
     }
 
     handleCheckout = () => {
-        this.props.history.push("/checkout");  
+        this.props.history.push("/checkout");
     }
 
-    componentDidMount() {
-        console.log(this.props);
-    }
 
 
     render() {
         return (
             <div>
                 <div className=" d-flex flex-md-row flex-column ">
-                    <Burger ingredients={this.state.ingredients} />
+                    <Burger ingredients={this.props.ingredients} />
                     <Controls
                         ingredientAdded={this.addIngredientHandle}
                         ingredientRemoved={this.removeIngredientHandle}
-                        price={this.state.totalPrice}
+                        price={this.props.totalPrice}
                         toggleModal={this.toggleModal}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.props.purchasable}
                     />
                 </div>
 
                 <Modal isOpen={this.state.modalOpen}>
                     <ModalHeader>Your Order Summary</ModalHeader>
                     <ModalBody>
-                        <h5>Total Price: {this.state.totalPrice.toFixed(0)} BDT</h5>
-                        <Summary ingredients={this.state.ingredients} />
+                        <h5>Total Price: {this.props.totalPrice.toFixed(0)} BDT</h5>
+                        <Summary ingredients={this.props.ingredients} />
                     </ModalBody>
                     <ModalFooter>
                         <Button color="success" onClick={this.handleCheckout}>Continue to  Checkout</Button>
@@ -104,3 +85,9 @@ export default class BurgerBuilder extends Component {
     }
 }
 
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(BurgerBuilder);
+
+// export default BurgerBuilder
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,4 @@
-
+import * as actionTypes from './actionTypes'
 const INGREDIENT_PRICE = {
     cheese: 20,
     salad: 40,
@@ -17,7 +17,41 @@ const INITIAL_STATE = {
 }
 
 export const reducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) { 
+    const ingredients = [...this.ingredients];
+    
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            for (let item of ingredients) {
+                if (item.type === action.payload) item.amount++;
+            }
+            return {
+                ...state,
+                ingredients: ingredients,
+                totalPrice: state.totalPrice + INGREDIENT_PRICE[action.payload],
+            }
+
+        case actionTypes.REMOVE_INGREDIENT:
+            for (let item of ingredients) {
+                if (item.type === action.payload) {
+                    if (item.amount <= 0) return state;
+                    item.amount--;
+                }
+            }
+            return {
+                ...state,
+                ingredients: ingredients,
+                totalPrice: state.totalPrice - INGREDIENT_PRICE[action.payload],
+            }
+        case actionTypes.UPDATE_PURCHASABLE:
+            const sum = state.ingredients.reduce((sum, element) => {
+                return sum + element.amount;
+            }, 0)
+            return {
+                ...state,
+                purchasable: sum > 0,
+            }
+
+
         default:
             return state;
     }
